e46a65309b97d1c923d3171175a056623237419f
samans <scott.mansfield@gmail.com>
2024-05-17 12:35:23 -0400

    refresh

--- ieee-yang-testing/ASdm/rp.sh
+++ ieee-yang-testing/ASdm/rp.sh
@@ -1,2 +1,2 @@
 
-pyang -p ~/yang/yang/standard/ietf/RFC -p ~/yang/yang/standard/ieee/draft/1588 -p ~/yang/yang/standard/iana -p ../ASdn ieee802-dot1as-hs.yang
+pyang -p ~/yang/yang/standard/ietf/RFC -p ~/yang/yang/standard/ieee/published/1588 -p ~/yang/yang/standard/iana -p ../ASdn ieee802-dot1as-hs.yang
--- ieee-yang-testing/Qdy/rp-mwp.sh
+++ ieee-yang-testing/Qdy/rp-mwp.sh
@@ -1,2 +1,2 @@
 
-pyang -p ~/yang/standard/ietf/RFC -p ~/yang/standard/ieee/published/802 -p ~/yang/standard/ieee/published/802.1 -p ~/yang/standard/iana *.yang 
+pyang --lint --ieee -p ~/yang/standard/ietf/RFC -p ~/yang/standard/ieee/published/802 -p ~/yang/standard/ieee/published/802.1 -p ~/yang/standard/iana *.yang 
