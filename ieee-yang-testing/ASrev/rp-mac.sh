ab43b756e4a8079d87435bc8dde932c82eae044a
Scott Mansfield <scott.mansfield@gmail.com>
2025-02-25 11:21:09 +0100

    Feb 2025, status check

--- ieee-yang-testing/ASrev/rp-mac.sh
+++ ieee-yang-testing/ASrev/rp-mac.sh
@@ -1,2 +1,2 @@
 
-pyang --ieee -p ~/gits/yang/standard/ietf/RFC -p ~/gits/yang/standard/ieee/published/802 -p ~/gits/yang/standard/ieee/published/802.1 -p ~/gits/yang/standard/ieee/published/1588 -p ~/gits/yang/standard/iana *.yang 
+pyang --verbose --strict --ieee -p D1.0 -p ~/gits/yang/standard/ietf/RFC -p ~/gits/yang/standard/ieee/published/802 -p ~/gits/yang/standard/ieee/published/802.1 -p ~/gits/yang/standard/ieee/published/1588 -p ~/gits/yang/standard/iana D1.0/*.yang 
