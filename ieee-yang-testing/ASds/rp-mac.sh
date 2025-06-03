ebb181c02137fdb9466f680cf34e1fad1463ceb1
Scott Mansfield <scott.mansfield@gmail.com>
2025-06-03 10:32:17 +0900

    2025-06-03 update

--- ieee-yang-testing/ASds/rp-mac.sh
+++ ieee-yang-testing/ASds/rp-mac.sh
@@ -1,2 +1,2 @@
 
-pyang --verbose --ieee -p ~/gits/yang/standard/ietf/RFC -p ~/gits/yang/standard/ieee/published/802 -p ~/gits/yang/standard/ieee/published/802.1 -p ~/gits/yang/standard/ieee/published/1588 -p ~/gits/yang/standard/iana ./D1.1/*.yang 
+pyang --verbose --ieee -p ~/gits/yang/standard/ietf/RFC -p ~/gits/yang/standard/ieee/published/802 -p ~/gits/yang/standard/ieee/published/802.1 -p ~/gits/yang/standard/ieee/published/1588 -p ~/gits/yang/standard/iana ./D1.2/*.yang 
