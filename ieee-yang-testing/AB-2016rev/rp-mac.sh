3dfa6d23a0ceb6598d0a8397518a23ee6c6db6b6
Scott Mansfield <scott.mansfield@gmail.com>
2025-09-09 11:36:02 -0400

    lag examples

--- ieee-yang-testing/AB-2016rev/rp-mac.sh
+++ ieee-yang-testing/AB-2016rev/rp-mac.sh
@@ -1,2 +1,2 @@
 
-pyang --lint --ieee -p ~/gits/yang/standard/ietf/RFC -p ~/gits/yang/standard/ieee/published/802 -p ~/gits/yang/standard/ieee/published/802.1 -p ~/gits/yang/standard/iana *.yang 
+pyang --lint --ieee -p ~/gits/yang/standard/ietf/RFC -p ~/gits/yang/standard/ieee/published/802 -p ~/gits/yang/standard/ieee/published/802.1 -p ~/gits/yang/standard/iana D0.2.1/*.yang 
