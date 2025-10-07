e38d91fea23c8c2c2ec9ee44cae978b0b866a62c
Scott Mansfield <scott.mansfield@gmail.com>
2025-10-07 07:40:03 -0400

    proof

--- ieee-yang-testing/AXdz/rp.sh
+++ ieee-yang-testing/AXdz/rp.sh
@@ -1,2 +1,2 @@
 
-pyang -p ~/yang/yang/standard/ietf/RFC -p ~/yang/yang/standard/ieee/published/802 -p ~/yang/yang/standard/iana *.yang
+pyang --max-line-length 68 -p ~/yang/yang/standard/ietf/RFC -p ~/yang/yang/standard/ieee/published/802 -p ~/yang/yang/standard/iana -p ./proof proof/*.yang
