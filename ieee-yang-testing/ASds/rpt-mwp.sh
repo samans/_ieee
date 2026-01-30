43b9044e1f70ea7b5ba3278bc2315cf0ce5237cc
Scott Mansfield <scott.mansfield@gmail.com>
2026-01-30 04:54:19 -0500

    2026-01 update and mib testing

--- ieee-yang-testing/ASds/rpt-mwp.sh
+++ ieee-yang-testing/ASds/rpt-mwp.sh
@@ -1,2 +1,1 @@
-
-pyang -f tree --ieee -p ~/yang/standard/ietf/RFC -p ~/yang/standard/ieee/published/802 -p ~/yang/standard/ieee/published/802.1 -p ~/yang/standard/ieee/published/1588 -p ~/yang/standard/iana -p ../ASdn -p ../ASdm ../ASdn/ieee802-dot1as-gptp.yang ../ASdm/ieee802-dot1as-hs.yang ./ieee802-dot1as-hd.yang
+pyang -f tree -o asds.tree -p ~/yang/standard/ietf/RFC -p ~/yang/standard/ieee/published/802 -p ~/yang/standard/ieee/published/802.1 -p ~/yang/standard/ieee/published/1588 -p ~/yang/standard/iana ./D2.0.1/*.yang 
--- ieee-yang-testing/AXdz/rp.sh
+++ ieee-yang-testing/AXdz/rp.sh
@@ -1,2 +1,2 @@
 
-pyang --max-line-length 68 -p ~/yang/yang/standard/ietf/RFC -p ~/yang/yang/standard/ieee/published/802 -p ~/yang/yang/standard/iana -p ./proof proof/*.yang
+pyang --lint --verbose --ieee -p ~/yang/yang/standard/ietf/RFC -p ~/yang/yang/standard/ieee/published/802 -p ~/yang/yang/standard/iana -p ./pub pub/*.yang
