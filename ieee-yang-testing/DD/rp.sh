40c31e0f0f8081408bb031f3ce331d61a6b06381
Scott Mansfield <scott.mansfield@gmail.com>
2026-03-02 16:50:50 -0500

    2026-02 update

--- ieee-yang-testing/DD/rp.sh
+++ ieee-yang-testing/DD/rp.sh
@@ -1,1 +1,1 @@
-pyang --lint --verbose --ieee -p ~/yang/standard/ietf/RFC -p ~/yang/standard/ieee/published/802 -p ~/yang/standard/iana -p ~/yang/standard/ieee/published/802.1 v1.4.1/*.yang
+pyang --lint --verbose --ieee -p ~/yang/standard/ietf/RFC -p ~/yang/standard/ieee/published/802 -p ~/yang/standard/iana -p ~/yang/standard/ieee/published/802.1 v1.4.posted/*.yang
